98350d662ea8817a21ae1e00091e7e8f990ace44
notpikmin <76235678+notpikmin@users.noreply.github.com>
2022-03-12 10:04:57 -0800

    Added DynamicBones editor.cs

---
+++ DynamicBones Editor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(DynamicBone))]
+public class DynamicBonesEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("This"))
+        {
+            Selection.activeGameObject.GetComponent<DynamicBone>().m_Root = Selection.activeGameObject.transform ;
+        }
+
+        DrawDefaultInspector();
+    }
+
+}
